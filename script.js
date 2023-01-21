98837668fb302639b5d0fad08fb73e5af5da6488
JayeshMalviya5 <115400817+JayeshMalviya5@users.noreply.github.com>
2023-01-21 21:19:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let addData = document.getElementById('add');
+addData.addEventListener('click',storeData);
+function storeData(){
+    let input = document.getElementById('inp');
+    let inputDate = document.getElementById('deadline');
+    let priority = document.getElementById('importance');
+    console.log(input.value);
+    console.log(inputDate.value);
+    console.log(input.value);
+    
+}
